ecd1b917e97edc61254d8d3f451f3711f8cb5045
Chesare22 <Chesare22@github.com>
2020-09-08 20:01:38 -0500

    refactor: Move DOM mainpulation to `Views.js`

--- View.js
+++ View.js
@@ -1,6 +1,6 @@
-import { mapMatrix } from './UsefulFunctions.js'
+import { mapMatrix, createMatrix } from './UsefulFunctions.js'
 
-const possibleCells = {
+const createCellContent = {
   '-1'() {
     const cellContent = document.createElement('img')
     cellContent.src = './assets/bomb.svg'
@@ -17,17 +17,49 @@ const possibleCells = {
     }
     return cellContent
   }),
+  flag() {
+    const flag = document.createElement('img')
+    flag.src = './assets/flag.svg'
+    flag.className = 'flag'
+    flag.width = 20
+    return flag
+  },
 }
 
 
-const createCells = game => mapMatrix(game.board, () => {
+const createCell = () => {
   const button = document.createElement('button')
   button.className = 'covered no-browser-styling'
-
   return button
-})
-
-export {
-  createCells,
-  possibleCells,
 }
+
+function View(board) {
+  const cells = mapMatrix(board, createCell)
+  const flags = createMatrix(board.length, board[0].length, null)
+
+  return {
+    cells,
+    uncoverCell([ x, y ]) {
+      const button = cells[x][y]
+      button.className = 'uncovered no-browser-styling'
+      if (!button.firstChild) {
+        const number = board[x][y]
+        const cellContent = createCellContent[number]()
+        button.appendChild(cellContent)
+      }
+    },
+    addFlag(x, y) {
+      const flagImg = flags[x][y] || createCellContent.flag()
+      flags[x][y] = flagImg
+      cells[x][y].appendChild(flagImg)
+    },
+    deleteFlag(x, y) {
+      const flagImg = flags[x][y]
+      if (flagImg) {
+        cells[x][y].removeChild(flagImg)
+      }
+    },
+  }
+}
+
+export default View
--- logic.js
+++ logic.js
@@ -1,51 +1,33 @@
 import Game from './Game.js'
-import { mapMatrix } from './UsefulFunctions.js'
-import { createCells, possibleCells } from './View.js'
+import View from './View.js'
 
 const game = Game(21, 23, 99)
-const createFlagImage = () => {
-  const flag = document.createElement('img')
-  flag.src = './assets/flag.svg'
-  flag.className = 'flag'
-  flag.width = 20
-  return flag
+const view = View(game.board)
+
+const handleClick = (row, column) => mouseEvent => {
+  if (mouseEvent.button === 0) {
+    // uncover button
+    game.pressUncoverButton(row, column)
+      .forEach(view.uncoverCell)
+  } else if (mouseEvent.button === 2) {
+    // flag button
+    const showFlag = game.pressFlagButton(row, column)
+    if (showFlag) {
+      view.addFlag(row, column)
+    } else {
+      view.deleteFlag(row, column)
+    }
+  }
 }
 
 window.onload = function() {
   const board = document.getElementById('board')
 
-  const cells = createCells(game)
-  const flags = mapMatrix(game.board, () => null)
-
-  const handleClick = (row, column) => mouseEvent => {
-    if (mouseEvent.button === 0) {
-      // uncover button
-      game.pressUncoverButton(row, column)
-        .forEach(([ x, y ]) => {
-          const button = cells[x][y]
-          button.className = 'uncovered no-browser-styling'
-          if (!button.firstChild) {
-            const cellContent = possibleCells[game.board[x][y]]()
-            button.appendChild(cellContent)
-          }
-        })
-    } else if (mouseEvent.button === 2) {
-      // flag button
-      const showFlag = game.pressFlagButton(row, column)
-      if (showFlag) {
-        const flagImg = flags[row][column] || createFlagImage()
-        flags[row][column] = flagImg
-        cells[row][column].appendChild(flagImg)
-      } else if (flags[row][column]) {
-        cells[row][column].removeChild(flags[row][column])
-      }
-    }
-  }
-  cells.forEach((row, rowIndex) => {
-    row.forEach((button, columnIndex) => {
-      board.appendChild(button)
-      button.addEventListener('contextmenu', e => { e.preventDefault(); return false })
-      button.addEventListener('mouseup', handleClick(rowIndex, columnIndex))
+  view.cells.forEach((row, rowIndex) => {
+    row.forEach((cell, columnIndex) => {
+      board.appendChild(cell)
+      cell.addEventListener('contextmenu', e => { e.preventDefault(); return false })
+      cell.addEventListener('mouseup', handleClick(rowIndex, columnIndex))
     })
   })
 }
