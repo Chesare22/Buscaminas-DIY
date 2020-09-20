2654e1d008c6735cb1164860f9b51f9d44b0a204
Chesare22 <Chesare22@github.com>
2020-09-20 02:14:09 -0500

    feat: printState

--- createNewGame.js
+++ createNewGame.js
@@ -35,22 +35,23 @@ const createNewGame = ({ rows, columns, mines }) => {
   let flagsLeft = mines
   let cellsLeft = (rows * columns) - mines
 
-  const board = placeMines({ rows, columns, mines })
-  const flags = createMatrix({ rows, columns, value: false })
-  const uncoveredCells = JSON.parse(JSON.stringify(flags))
+  const state = {
+    board: placeMines({ rows, columns, mines }),
+    flags: createMatrix({ rows, columns, value: false }),
+    uncoveredCells: createMatrix({ rows, columns, value: false }),
+  }
 
-  console.log('board:', board)
 
   const recursiveUncover = (row, column, coordinates) => {
-    if (flags[row][column] || uncoveredCells[row][column]) {
+    if (state.flags[row][column] || state.uncoveredCells[row][column]) {
       return coordinates
     }
 
     --cellsLeft
-    uncoveredCells[row][column] = true
-    coordinates.push([row, column])
+    state.uncoveredCells[row][column] = true
+    coordinates.push({ row, column })
 
-    if (board[row][column] === 0) {
+    if (state.board[row][column] === 0) {
       for (let i = Math.max(0, row - 1); i < Math.min(rows, row + 2); i++) {
         for (let j = Math.max(0, column - 1); j < Math.min(columns, column + 2); j++) {
           recursiveUncover(i, j, coordinates)
@@ -65,29 +66,35 @@ const createNewGame = ({ rows, columns, mines }) => {
     get gameOver() { return gameOver },
     get victory() { return cellsLeft === 0 },
 
+    printState(keys = ['board', 'flags', 'uncoveredCells']) {
+      keys.forEach(key => {
+        if (state[key] != null) {
+          console.log(`${key}:`, state[key])
+        }
+      })
+    },
+
     flag({ row, column }) {
-      if (uncoveredCells[row][column]) {
+      if (state.uncoveredCells[row][column]) {
         return null
       }
 
-      flags[row][column] = !flags[row][column]
-      flagsLeft += flags[row][column] ? 1 : -1
+      state.flags[row][column] = !state.flags[row][column]
+      flagsLeft += state.flags[row][column] ? 1 : -1
       return {
-        hasFlag: flags[row][column],
+        hasFlag: state.flags[row][column],
         flagsLeft,
       }
     },
 
     // Returns an array of coordinates that should be uncovered
     uncover({ row, column }) {
-      if (board[row][column] !== -1) {
+      if (state.board[row][column] === -1) {
         gameOver = true
-        return [[row, column]]
+        return [{ row, column }]
       }
 
       return recursiveUncover(row, column, [])
     },
   }
 }
-
-// export default Game
