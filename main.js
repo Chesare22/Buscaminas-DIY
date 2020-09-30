79f144545ba196d1f9deadf15bfd32aa06802a9e
Chesare22 <Chesare22@github.com>
2020-09-30 03:48:27 -0500

    docs: Add simple comments

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 /* global drawBoard, createNewGame, configHelper */
 
 window.onload = function() {
+  // ----- DOM elements -----
   const board = document.getElementById('board')
   const remainingFlags = document.getElementById('flags')
   const difficultyDropdown = document.getElementById('difficulty-dropdown')
 
+  // ----- Global config -----
   let config = configHelper.initialConfig
 
+  // ----- Init dependencies and declare useful functions -----
   remainingFlags.innerText = config.mines
   let minesweeper
   const boardUI = drawBoard({
@@ -14,6 +17,9 @@ window.onload = function() {
     columns: config.columns,
     cellWidth: config.cellWidth,
     element: board,
+  })
+  const difficultySelector = configHelper.createDifficultySelector({
+    element: difficultyDropdown,
   })
 
   function resetGame() {
@@ -27,14 +33,15 @@ window.onload = function() {
   }
 
 
-  const difficultySelector = configHelper.createDifficultySelector({
-    element: difficultyDropdown,
-  })
+  // ----- Event listeners -----
+
+  // When the player changes the difficulty
   difficultySelector.addListener(newConfig => {
     config = newConfig
     resetGame()
   })
 
+  // When the player clicks on the board
   board.addEventListener('contextmenu', e => { e.preventDefault(); return false })
   board.addEventListener('mouseup', mouseEvent => {
     const coordinates = boardUI.getCoordinates(mouseEvent)
